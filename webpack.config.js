1be03c2caafa7a95e8be2fed0b3a6796e841552e
Amanda Flink <amanda@Amandas-MacBook-Pro-2.local>
2019-07-02 12:25:12 -0400

    my first commit... cont

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './client/index.js',
+    plugins: [ //plugins for separating css in bundle (during prod) and HMR
+        new MiniCssExtractPlugin({}),
+        new webpack.HotModuleReplacementPlugin()
+      ],
+      mode: process.env.NODE_ENV,
+      output: {
+        path: path.join(__dirname, '/build'), //puts things in a build folder
+        filename: 'bundle.js', //under the name bumdle
+      },
+      devServer: {
+        publicPath: '/build/', //for dev, proxy kicks back to localhost 3000
+        proxy: {
+          '/': 'http://localhost:3000'
+        }
+      },
+      module: {
+        rules: [
+          {
+            test: /\.jsx?$/, //for js and jsx files
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader', //use babel-loader
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+              },
+            },
+          },
+          {
+            test: /.s?css$/, //for scss and css
+            exclude: /node_modules/,
+            use: [
+              // choose MiniCss plugin for prod, style-loader for dev
+              process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+              "css-loader", // translates CSS into CommonJS
+              "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+          }
+        ]
+      }
+}
